f980c7d2e1f3e14994023fb7b9239367f445bd77
root <root@DESKTOP-RH905M8.localdomain>
2022-01-03 12:27:37 +0900

    [fix]:css有効化

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,10 @@ module.exports = {
   },
   module:{
     rules:[
-      // {
-      //   test:/\.css$/,
-      //   use:['style-loader','css-loader'],
-      // },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader'],
+      },
       {
         test:/\.(tsx|ts)$/,
         use:[
